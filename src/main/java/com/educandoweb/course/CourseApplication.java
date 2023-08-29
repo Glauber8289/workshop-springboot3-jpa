3e44e04ceb66dd808d0752a38da28e58f8f708f7
Glauber Peterson <107453279+Glauber8289@users.noreply.github.com>
2023-08-28 23:51:57 -0300

    Atualizar o CourseApplication.java

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -8,7 +8,7 @@ public class CourseApplication {
 
 	public static void main(String[] args) {
 
-	//classe principal	SpringApplication.run(CourseApplication.class, args);
+		SpringApplication.run(CourseApplication.class, args);
 	}
 
 }
