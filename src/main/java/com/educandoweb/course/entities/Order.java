176a97775c9e5b636a29d444d220ad5becc7e2ed
Glauber8289 <Petersonglauber@hotmail.com>
2023-09-07 23:00:15 -0300

    Order, Instant, ISO 8601 - Part 2 Ajustando e finalizando

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
@@ -23,9 +24,11 @@ import jakarta.persistence.Table;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@JsonFormat (shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd' T 'HH:mm:ss'Z'", timezone="GMT")
 	private Instant moment;
 
-	@JsonIgnore
+	
 	@ManyToOne
 	@JoinColumn(name="client_id")
 	private User client;
