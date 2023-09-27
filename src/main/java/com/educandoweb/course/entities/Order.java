3314c8ee1c0274d1afa6ec873c41925f4c2f0c77
Glauber8289 <Petersonglauber@hotmail.com>
2023-09-26 22:53:32 -0300

    Order Item, many-to-many association with extra atributes PART2

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -14,6 +16,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -34,7 +37,14 @@ import jakarta.persistence.Table;
 	@JoinColumn(name="client_id")
 	private User client;
 	
-		public Order() {
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items= new HashSet<>();
+	
+	public Set <OrderItem> getitems(){
+		return items;
+	}
+		
+	public Order() {
 
 	}
 	
