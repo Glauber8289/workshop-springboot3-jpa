b9f90a388d725f1382e7a9b8c7a97ccb3c3af575
Glauber Peterson <107453279+Glauber8289@users.noreply.github.com>
2023-10-18 09:16:02 -0300

    Update TestConfig.java

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -36,12 +36,12 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired 
 	private ProductRepository productRepository;
-    @Autowired
-    private OrderItemRepository orderItemRepository;
+        @Autowired
+        private OrderItemRepository orderItemRepository;
 	@Override
 	public void run(String... args) throws Exception {
 		//Criando categorias
-	    Category cat1 = new Category(null, "Electronics"); 
+	        Category cat1 = new Category(null, "Electronics"); 
 		Category cat2 = new Category(null, "Books"); 
 		Category cat3 = new Category(null, "Computers"); 
 		//Criando produtos
@@ -59,9 +59,9 @@ public class TestConfig implements CommandLineRunner {
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p2.getCategories().add(cat3);
-        p3.getCategories().add(cat3);
-        p4.getCategories().add(cat3);
-        p5.getCategories().add(cat2);
+                p3.getCategories().add(cat3);
+                p4.getCategories().add(cat3);
+                p5.getCategories().add(cat2);
         
         productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
         
